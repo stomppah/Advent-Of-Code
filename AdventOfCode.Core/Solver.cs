74727c1463298a9177ab64d04b8306a7b70243e0
stomppah@SITHIS <two.woods@gmail.com>
2017-12-05 18:23:44 +0000

    Day two puzzles solved!!!

--- AdventOfCode.Core/Solver.cs
+++ AdventOfCode.Core/Solver.cs
@@ -48,5 +48,77 @@ namespace AdventOfCode.Core
 
             return matches.Sum();
         }
+
+        public double CalculateChecksum(string spreadsheet)
+        {
+            char newLine = Convert.ToChar(13);
+            char tab = Convert.ToChar(9);
+
+            string[] rowdata = spreadsheet.Split(newLine);
+
+            List<long[]> table = new List<long[]>();
+            long[] rowLongData;
+            for (int i = 0; i <= rowdata.Length-1; i++)
+            {
+                rowdata[i] = rowdata[i].TrimStart('\n');
+                var rowStringData = rowdata[i].Contains('\t') ? rowdata[i].Split('\t') : rowdata[i].Split(' ');
+                rowLongData = new long[rowStringData.Length];
+                for (int j = 0; j <= rowStringData.Length-1; j++)
+                {
+                    Int64.TryParse(rowStringData[j], out rowLongData[j]);
+                }
+                table.Add(rowLongData);
+            }
+
+            double runningTotal = 0;
+            foreach (var row in table)
+            {
+                runningTotal += row.Max() - row.Min();
+            }
+
+            return runningTotal;
+        }
+
+        public double CalculateChecksumExtended(string spreadsheet)
+        {
+            char newLine = Convert.ToChar(13);
+            char tab = Convert.ToChar(9);
+
+            string[] rowdata = spreadsheet.Split(newLine);
+
+            List<long[]> table = new List<long[]>();
+            long[] rowLongData;
+            for (int i = 0; i <= rowdata.Length - 1; i++)
+            {
+                rowdata[i] = rowdata[i].TrimStart('\n');
+                var rowStringData = rowdata[i].Contains('\t') ? rowdata[i].Split('\t') : rowdata[i].Split(' ');
+                rowLongData = new long[rowStringData.Length];
+                for (int j = 0; j <= rowStringData.Length - 1; j++)
+                {
+                    Int64.TryParse(rowStringData[j], out rowLongData[j]);
+                }
+                table.Add(rowLongData);
+            }
+
+            double runningTotal = 0;
+            foreach (var row in table)
+            {
+                double divisionSum = 0;
+                for (int rowIndex = 0; rowIndex < row.Length; rowIndex++)
+                {
+                    for (int nestedIndex = 0; nestedIndex < row.Length; nestedIndex++)
+                    {
+                        if (row[rowIndex] % row[nestedIndex] == 0 && rowIndex != nestedIndex)
+                        {
+                            // ReSharper disable once PossibleLossOfFraction
+                            divisionSum += row[rowIndex] / row[nestedIndex];
+                        }
+                    }
+                }
+                runningTotal += divisionSum;
+            }
+
+            return runningTotal;
+        }
     }
 }
---
+++ AdventOfCode.Tests/DayTwoTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Core;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class DayTwoTests
+    {
+        [Test]
+        public void PartOne()
+        {
+            string spreadsheet =
+@"5 1 9 5
+7 5 3
+2 4 6 8";
+            var result = new Solver().CalculateChecksum(spreadsheet);
+
+            Assert.AreEqual(18, result);
+        }
+
+        [Test]
+        public void PartTwo()
+        {
+            string spreadsheet =
+@"5 9 2 8
+9 4 7 3
+3 8 6 5";
+            var result = new Solver().CalculateChecksumExtended(spreadsheet);
+
+            Assert.AreEqual(9, result);
+        }
+
+    }
+}
