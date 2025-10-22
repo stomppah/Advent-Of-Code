16be8e3af3a7211a608fdd94a36e467aa685cadd
stomppah <two.woods@gmail.com>
2025-10-22 17:10:19 +0100

    Start building out some logic

--- AdventOfCode/AdventOfCode/2017/SpiralGrid.cs
+++ AdventOfCode/AdventOfCode/2017/SpiralGrid.cs
@@ -8,8 +8,17 @@ namespace AdventOfCode._2017
     {
         public static int[] BuildValues(int n)
         {
-            
-            return new int[0];
+            if (n <= 0) throw new ArgumentException("Input must be a positive integer.", nameof(n));
+
+            var values = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                values[i] = i + 1; // Example logic: fill with sequential numbers starting from 1
+            }
+
+
+            return new int[n];
         }
     }
 }
